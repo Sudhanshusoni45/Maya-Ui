1672917081f4f6c84216679a92df9e6a99cd9991
Sudhanshusoni45 <sudhanshusoni45@gmail.com>
2022-04-08 12:15:06 +0530

    feat: add basic modal

--- Pages/Modal/modal.js
+++ Pages/Modal/modal.js
@@ -2,16 +2,18 @@ const modal = document.querySelector(".modal");
 const showModalBtn = document.querySelector(".modal-btn");
 const closeModalBtn = document.querySelector(".modal-cross");
 const modalBackdrop = document.querySelector(".modal-backdrop-hide");
+const modalContainer = document.querySelector(".modal-container");
 
-showModalBtn.addEventListener("click", showModal);
+showModalBtn.addEventListener("click", toggleModal);
 closeModalBtn.addEventListener("click", closeModal);
 
-function showModal() {
+function toggleModal() {
   modal.classList.toggle("modal-hide");
   modalBackdrop.classList.toggle("modal-backdrop-hide");
-  console.log("modalBackdrop:", modalBackdrop);
+  modalContainer.classList.toggle("modal-container-hide");
 }
 function closeModal() {
   modalBackdrop.classList.toggle("modal-backdrop-hide");
+  modalContainer.classList.toggle("modal-container-hide");
   modal.classList.toggle("modal-hide");
 }
